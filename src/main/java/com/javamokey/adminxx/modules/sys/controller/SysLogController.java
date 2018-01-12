d4a74e4f6ccf443eedf64813ad20779af5ada255
Cheney <30592850+chenguoo@users.noreply.github.com>
2018-01-12 17:53:06 +0800

    add 操作日志查询

--- src/main/java/com/javamokey/adminxx/modules/sys/controller/SysLogController.java
+++ src/main/java/com/javamokey/adminxx/modules/sys/controller/SysLogController.java
@@ -1,9 +1,25 @@
 package com.javamokey.adminxx.modules.sys.controller;
 
 
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.baomidou.mybatisplus.plugins.Page;
+import com.baomidou.mybatisplus.plugins.pagination.Pagination;
+import com.javamokey.adminxx.common.util.PageUtils;
+import com.javamokey.adminxx.common.util.R;
+import com.javamokey.adminxx.common.util.StringUtil;
+import com.javamokey.adminxx.modules.sys.entity.SysLog;
+import com.javamokey.adminxx.modules.sys.entity.vo.SysRoleVo;
+import com.javamokey.adminxx.modules.sys.service.SysLogService;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -13,9 +29,41 @@ import org.springframework.stereotype.Controller;
  * @author Cheney
  * @since 2017-12-26
  */
-@Controller
-@RequestMapping("/sys/sysLog")
+@RestController
+@RequestMapping("/sys/log")
 public class SysLogController {
 
+    private SysLogService sysLogService;
+
+    public SysLogController(SysLogService sysLogService) {
+        this.sysLogService = sysLogService;
+    }
+
+
+    /**
+     * 日志列表
+     *
+     * @param params
+     * @return
+     */
+    @RequestMapping("/list")
+    @RequiresPermissions("sys:log:list")
+    public R list(Pagination params) {
+        //查询列表数据
+        Page<SysLog> page = new Page<>(params.getCurrent(), params.getSize());
+
+        EntityWrapper<SysLog> wrapper = new EntityWrapper<>();
+
+//        if(!StringUtil.isEmpty(key)){
+//            wrapper.eq("username", key).or().eq("operation", key);
+//        }
+        wrapper.orderBy("create_date", false);
+
+
+        page = sysLogService.selectPage(page,wrapper);
+
+        return R.ok().put("page", page);
+
+    }
 }
 
---
+++ src/main/resources/static/pages/modules/sys/log.js
@@ -0,0 +1,61 @@
+$(function () {
+    $("#jqGrid").jqGrid({
+        url: baseURL + 'sys/log/list',
+        datatype: "json",
+        colModel: [			
+			{ label: 'id', name: 'id', width: 30, key: true },
+			{ label: '用户名', name: 'username', width: 50 }, 			
+			{ label: '用户操作', name: 'operation', width: 70 }, 			
+			{ label: '请求方法', name: 'method', width: 150 }, 			
+			{ label: '请求参数', name: 'params', width: 80 },
+            { label: '执行时长(毫秒)', name: 'time', width: 80 },
+			{ label: 'IP地址', name: 'ip', width: 70 }, 			
+			{ label: '创建时间', name: 'createDate', width: 90 }			
+        ],
+		viewrecords: true,
+        height: 385,
+        rowNum: 10,
+		rowList : [10,30,50],
+        rownumbers: true, 
+        rownumWidth: 25, 
+        autowidth:true,
+        multiselect: false,
+        pager: "#jqGridPager",
+        jsonReader : {
+            root: "page.records",//返回当前记录列表
+            page: "page.current",//返回当前第几页
+            total: "page.pages",//返回总页数
+            records: "page.total"//返回总记录数
+        },
+        prmNames : {
+            page:"current",//请求当前第几页
+            rows:"size",//请求每页条数
+            order: "order"//排序
+        },
+        gridComplete:function(){
+        	//隐藏grid底部滚动条
+        	$("#jqGrid").closest(".ui-jqgrid-bdiv").css({ "overflow-x" : "hidden" }); 
+        }
+    });
+});
+
+var vm = new Vue({
+	el:'#rrapp',
+	data:{
+		q:{
+			key: null
+		},
+	},
+	methods: {
+		query: function () {
+			vm.reload();
+		},
+		reload: function (event) {
+			var page = $("#jqGrid").jqGrid('getGridParam','page');
+			$("#jqGrid").jqGrid('setGridParam',{ 
+				postData:{'key': vm.q.key},
+                page:page
+            }).trigger("reloadGrid");
+		}
+	}
+});
